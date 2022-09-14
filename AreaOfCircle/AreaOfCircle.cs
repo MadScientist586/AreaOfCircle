4c9469300379cfb43fce615b8fdeab848ba1f17e
MadScientist586 <mr.scientist.knight@gmail.com>
2022-09-14 12:01:28 -0500

    Made area of circle calculator

---
+++ AreaOfCircle/AreaOfCircle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfCircle
+{
+    internal class AreaOfCircle
+    {
+
+
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double radius;
+
+            Console.WriteLine("Enter a number:");
+
+            radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(radius * radius * pi);
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+    
+}
